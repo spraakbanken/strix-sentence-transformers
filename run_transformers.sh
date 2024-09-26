b60939e4b7342685678ef2021e3155df7caf80f2
Maria Öhrman <maria.ohrman@gu.se>
2024-09-26 11:37:24 +0200

    improvements to script

--- run_transformers.sh
+++ run_transformers.sh
@@ -1,19 +1,23 @@
+logfile=~/logs/$(date '+%Y-%m-%d_%H%M%S').log
 
-mkdir -p /var/tmp/strix
-cd /var/tmp/strix
+echo "Running strix-sentence-transformers on $1" >> $logfile
 
-mkdir -p data
+
+mkdir -p /var/tmp/strix/envs
+cd /var/tmp/strix/envs
 
 tmp_dir=$(uuidgen)
 
 mkdir $tmp_dir
  
-git clone https://github.com/spraakbanken/strix-sentence-transformers.git $tmp_dir &> /dev/null
+git clone https://github.com/spraakbanken/strix-sentence-transformers.git $tmp_dir &>> $logfile
 cd $tmp_dir
-echo "transformers_postprocess_dir: ../data" > config.yml
+echo "transformers_postprocess_dir: ../../data" > config.yml
 python -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt &> /dev/null
+pip install -r requirements.txt &>> $logfile
 
-python main.py $1
+python main.py $1 &>> $logfile
 
+cd ..
+rm -rf $tmp_dir
